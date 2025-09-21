5869b368f9ad26c5d832b4ec2207bcf3fdd4a05b
Alexandr Milashensky <milashensky@gmail.com>
2025-09-21 15:27:52 +0300

    disable no-undef for config file

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig((mode) => {
+    // eslint-disable-next-line no-undef
     const env = loadEnv(mode, process.cwd(), '')
     const base = env.VITE_BASE_PATH || '/'
     return {
